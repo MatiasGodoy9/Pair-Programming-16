3c2c90128732c1d7d0cdb9ace4fc58a85ec8dcca
Godoy Matias <pm_godoy@hotmail.com>
2022-11-07 16:23:26 -0300

    Pair 16

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Garage from './components/Car';
+import App from './App';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Garage />
-  </React.StrictMode>
+    <App />
 );
